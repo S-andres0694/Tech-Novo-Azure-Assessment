dc830e2bf09040ca9c58e71eca69b07d18c837c8
S-andres0694 <sebasandres0694@gmail.com>
2025-05-19 15:08:06 +0100

    Update routes and add fs-routes dependency for improved routing functionality

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,9 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, route } from "@react-router/dev/routes";
+import { flatRoutes } from "@react-router/fs-routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  route("/", "./home.tsx"),
+  route("/about", "./about.tsx"),
+
+  ...(await flatRoutes()),
+] satisfies RouteConfig;
