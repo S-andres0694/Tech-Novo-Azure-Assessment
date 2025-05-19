23de0ab06112de93b9e47f33b48b34ea9ff82b9d
S-andres0694 <sebasandres0694@gmail.com>
2025-05-19 16:27:25 +0100

    Refactor routes to use updated file paths and remove unused fs-routes import

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,5 @@
 import { type RouteConfig, route } from "@react-router/dev/routes";
-import { flatRoutes } from "@react-router/fs-routes";
 
 export default [
-  route("/", "./home.tsx"),
-  route("/about", "./about.tsx"),
-
-  ...(await flatRoutes()),
+  route("/", "./routes/home.tsx"),
 ] satisfies RouteConfig;
