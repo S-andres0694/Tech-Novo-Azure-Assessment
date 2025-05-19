f442689a97720eb1b40a4caed48a2cff8af5d4b2
S-andres0694 <sebasandres0694@gmail.com>
2025-05-19 16:44:25 +0100

    Add ESLint and Prettier configuration files, update package dependencies, and set up Husky for pre-commit linting

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,3 @@
 import { type RouteConfig, route } from "@react-router/dev/routes";
 
-export default [
-  route("/", "./routes/home.tsx"),
-] satisfies RouteConfig;
+export default [route("/", "./routes/home.tsx")] satisfies RouteConfig;
---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  // @ts-expect-error: This is a recommended config by the documentation followed in: https://eslint.org/docs/latest/use/getting-started
+  // I just added the specific react version to avoid warnings issued by the VSCode Server.
+  tseslint.configs.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "19.1.0",
+      },
+    },
+  },
+  eslintConfigPrettier,
+]);
